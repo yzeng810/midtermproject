de4f0653be13f1aed2bd8cd821eb9503ff24b702
yzeng810 <47185646+yzeng810@users.noreply.github.com>
2019-03-13 01:49:03 -0400

    update

---
+++ resort/AmanLeMelezin.php
@@ -0,0 +1,13 @@
+<?php $title = 'Aman Summer Palace | Beijing'; ?>
+<?php $style = 'css/resort.css'; ?>
+<?php $top = 'css/top.css'; ?>
+<?php $css = 'sss/sss.css'; ?>
+<?php include "inc/top.php"; ?>	
+<?php include "inc/nav.php";?>
+
+
+
+<?php include "inc/js.php"; ?>
+
+</body>
+</html>
--- resort/AmanSummerPalace.php
+++ resort/AmanSummerPalace.php
@@ -3,9 +3,78 @@
 <?php $top = 'css/top.css'; ?>
 <?php $css = 'sss/sss.css'; ?>
 <?php include "inc/top.php"; ?>	
-<?php include "inc/nav.php";?>
+<?php include "inc/nav.php"; ?>
+<?php include "inc/resort-nav.php"; ?>
 
+<main>
 
+	<section class="full-width-slider">
+		<figure>
+		<img src="">
+		<figcaption>AMAN SUMMER PALACE | BEIJING </figcaption>
+		</figure>
+		<img src="">
+	</section>
+
+	<section class="aboutresort">
+		<h2>Explore The Resort</h2>
+			<p></p>
+		<section class="resortcomp">
+			<section class="adventures">
+				<figure>
+					<img src="">
+					<figcaption>Adventures</figcaption>
+				</figure>
+				<p></p>
+			</section>
+			<section class="dinning">
+				<figure>
+					<img src="">
+					<figcaption>Dinning</figcaption>
+				</figure>
+				<p></p>
+			</section>
+			<section class="accommodation">
+				<figure>
+					<img src="">
+					<figcaption>Accommodation</figcaption>
+				</figure>
+				<p></p>
+			</section>
+		</section>
+	</section>
+
+	<section class="unique">
+		<h2>One of A Kind</h2>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</section>
+
+	<section class="moreinfo">
+	<h2>More Info</h2>
+		<section class="destination">
+			<figure>
+				<img src="">
+				<figcaption>BEIJING</figcaption>
+			</figure>
+			<p></p>
+		</section>
+		<section class="brand">
+			<figure>
+				<img src="">
+				<figcaption>Aman</figcaption>
+			</figure>
+			<p></p>
+		</section>
+		<section class="reserve">
+			<p>Reserve Now</p>
+		</section>
+	</section>
+
+</main>
 
 <?php include "inc/js.php"; ?>
 
