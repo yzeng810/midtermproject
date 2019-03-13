4a6b1505314dee259e014a39ba15540db24a5916
Yuqi Zeng <yuqizeng@YuqideMacBook.local>
2019-03-13 12:19:34 -0400

    update

--- resort/AmanSummerPalace.php
+++ resort/AmanSummerPalace.php
@@ -1,45 +1,44 @@
 <?php $title = 'Aman Summer Palace | Beijing'; ?>
-<?php $style = 'css/resort.css'; ?>
-<?php $top = 'css/top.css'; ?>
-<?php $css = 'sss/sss.css'; ?>
-<?php include "inc/top.php"; ?>	
-<?php include "inc/nav.php"; ?>
-<?php include "inc/resort-nav.php"; ?>
+<?php $style = '../css/resort.css'; ?>
+<?php $top = '../css/top.css'; ?>
+<?php $css = '../sss/sss.css'; ?>
+<?php include "../inc/top.php"; ?>	
+<?php include "../inc/nav.php"; ?>
+<?php include "../inc/resort-nav.php"; ?>
 
 <main>
 
-	<section class="full-width-slider">
+	<section class="full-width">
 		<figure>
-		<img src="">
+		<img src="images/aman.jpg">
 		<figcaption>AMAN SUMMER PALACE | BEIJING </figcaption>
 		</figure>
-		<img src="">
 	</section>
 
 	<section class="aboutresort">
-		<h2>Explore The Resort</h2>
-			<p></p>
+		<h2>Discover The Resort</h2>
+			<p>Just a few steps from the East Gate of the Summer Palace grounds lies Aman at Summer Palace - the ideal location from which to discover both the history and energy of the Chinese capital. Housed in a series of pavilions, the resort is only 15km from the Forbidden City. A serene retreat, the property provides easy access to such cultural landmarks as the Great Wall, The Temple of Heaven and the hutongs, as well as Beijing's restaurants, art galleries and other contemporary delights. Aman Summer Palace offers a selection of heritage-imbued dwellings, including those once used by guests awaiting an audience with the Empress</p>
 		<section class="resortcomp">
 			<section class="adventures">
 				<figure>
-					<img src="">
+					<img src="images/aman3.jpg">
 					<figcaption>Adventures</figcaption>
 				</figure>
-				<p></p>
+				<p>One of the world’s richest repositories of ancient monuments, Beijing is home to six UNESCO World Heritage Sites, including the 260-year-old Summer Palace, the gates of which sit at Aman Summer Palace’s doorstep. In addition to the palace’s sculpted grounds and masterful architecture, nearby opportunities for discovery can be found at the Temple of Heaven, the Great Wall, the Forbidden City and in the maze of artists’ workshops spread across the metropolis.</p>
 			</section>
 			<section class="dinning">
+				<p>pread across the heritage-imbued grounds of Aman Summer Palace, the restaurants serve cuisine from across the continent and globe: the Ming Dynasty-inspired Chinese Restaurant features Cantonese cuisine as well as Imperial dishes; the Japanese Restaurant's set menus offer French-Japanese fusion; and the Grill is home to prime cuts of meat cooked with western techniques. Meals can also be arranged in private dining rooms.</p>
 				<figure>
-					<img src="">
+					<img src="images/aman4.jpg">
 					<figcaption>Dinning</figcaption>
 				</figure>
-				<p></p>
 			</section>
 			<section class="accommodation">
 				<figure>
-					<img src="">
+					<img src="images/aman5.jpg">
 					<figcaption>Accommodation</figcaption>
 				</figure>
-				<p></p>
+				<p>Rooms and Suites surround serene garden courtyards crisscrossed by stately pathways. Established indigenous trees cast delicate shadows through bamboo blinds and into the lofty-ceilinged Suites. Polished to a deep lustre, floors are finished in Jin clay tiles, while ceilings show exposed wooden roof beams. Bathrooms are generously proportioned with free-standing island bathtubs.</p>
 			</section>
 		</section>
 	</section>
@@ -47,24 +46,48 @@
 	<section class="unique">
 		<h2>One of A Kind</h2>
 		<ul>
-			<li></li>
-			<li></li>
-			<li></li>
+			<li>Ideal Location</li>
+			<li>Private Access</li>
+			<li>Living at Chinese Palace</li>
+			<li>Unique Property</li>
+			<li>Indoor pool</li>
+			<li>Special Entrance</li>
+			<li>Exceptional Services</li>
+			<li>Free High Speed Internet</li>
+			<li>Authentic Historic Chinese Experience</li>
 		</ul>
+	</section>
+
+	<section class="gallery">
+		<img src="images/aman1.jpg">
+		<img src="images/aman2.jpg">
+		<img src="images/aman6.jpg">
+		<img src="images/aman7.jpg">
+		<img src="images/aman8.jpg">
+		<img src="images/aman9.jpg">
+		<img src="images/aman10.jpg">
+		<img src="images/aman11.jpg">
+		<img src="images/aman12.jpg">
+		<img src="images/aman13.jpg">
+		<img src="images/aman14.jpg">
+		<img src="images/aman15.jpg">
+		<img src="images/aman16.jpg">
+		<img src="images/aman17.jpg">
+		<img src="images/aman18.jpg">
 	</section>
 
 	<section class="moreinfo">
 	<h2>More Info</h2>
 		<section class="destination">
 			<figure>
-				<img src="">
+				<img src="images/beijing.jpg">
 				<figcaption>BEIJING</figcaption>
 			</figure>
 			<p></p>
 		</section>
 		<section class="brand">
 			<figure>
-				<img src="">
+				<img src="images/aman0.jpg">
 				<figcaption>Aman</figcaption>
 			</figure>
 			<p></p>
