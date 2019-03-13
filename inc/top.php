93ac7f79b596a8593f82c1fa0f7103f178c35478
Yuqi Zeng <yuqizeng@YuqideMacBook.local>
2019-03-13 00:24:54 -0400

    update

---
+++ inc/top.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title> <?php echo($title); ?></title>
+	<link rel="stylesheet" href="<?php echo($style); ?>">
+	<link rel="stylesheet" href="<?php echo($top); ?>">
+	<link rel="stylesheet" href="<?php echo($css); ?>">
+</head>
+
+<body>
+	<header>
+		<a href="index.php">
+			<h1>World's Best Resorts</h1>
+		</a>
+		<sub>selected by Yuqi Zeng</sub>
+	</header>
